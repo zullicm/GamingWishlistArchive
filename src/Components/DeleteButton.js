c1250778d50b845ee42cf7064e5c0f3c8bc8c5dc
Christopher Marzulli <chriszulli15@gmail.com>
2022-07-19 12:37:28 -0400

    Game deletes now to update the dom

--- src/Components/DeleteButton.js
+++ src/Components/DeleteButton.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-function DeleteButton(){
+function DeleteButton({ id, onGameDelete }){
 
   function deleteGame(){
-    
+    fetch(`http://localhost:3000/games/${id}`, {
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(() => onGameDelete(id))
   }
   
   return(
-    <p className="delete-game" >Delete Game<i class="material-icons">delete</i></p>
+    <p onClick={deleteGame} className="delete-game" >Delete Game<i class="material-icons">delete</i></p>
   )
 }
 
