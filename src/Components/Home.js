e2c21db217f223ae0052382b9fca79e7291400a5
Christopher Marzulli <chriszulli15@gmail.com>
2022-07-21 15:07:43 -0400

    adding the homepage, what is this

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,14 @@ import React from "react";
 function Home(){
   return(
     <div>
-      <h1>This will be the HomePage</h1>
+      <h1>Welcome to Gaming Library!</h1>
+      <div id="homepage-info">
+        <h3>What is this?</h3>
+        <p className="home-info">Gaming library is a website to add games that you have played to keep track of what you have played. You can keep track of what you played them on, what you would rate them, and give them a fun short description or review.</p>
+        <br/>
+
+
+      </div>
     </div>
   )
 }
