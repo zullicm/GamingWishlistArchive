f55c40790c0ea38b9de34dc7aa508fd38376409f
Christopher Marzulli <chriszulli15@gmail.com>
2022-07-21 15:08:03 -0400

    finished, what is this

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,6 +8,7 @@ function Home(){
         <h3>What is this?</h3>
         <p className="home-info">Gaming library is a website to add games that you have played to keep track of what you have played. You can keep track of what you played them on, what you would rate them, and give them a fun short description or review.</p>
         <br/>
+        <p className="home-info">You also have a favorites tab to keep track of all the games you decide are your favorite ones, and a wihslist where you can submit games to keep them in mind next time you're looking for a new game.</p>
 
 
       </div>
