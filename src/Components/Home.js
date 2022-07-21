8fec9c880d8c9ddf235558f9a0a31d60d017c66f
Christopher Marzulli <chriszulli15@gmail.com>
2022-07-21 15:08:27 -0400

    adding how to use

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,10 @@ function Home(){
         <p className="home-info">Gaming library is a website to add games that you have played to keep track of what you have played. You can keep track of what you played them on, what you would rate them, and give them a fun short description or review.</p>
         <br/>
         <p className="home-info">You also have a favorites tab to keep track of all the games you decide are your favorite ones, and a wihslist where you can submit games to keep them in mind next time you're looking for a new game.</p>
-
+        <h3>How do you use it?</h3>
+        <p className="home-info">The process is very simple, click on the tab you wish to be directed too, and fill out the forms so and they'll be added to your lists.
+        </p>
+        <br/>
 
       </div>
     </div>
